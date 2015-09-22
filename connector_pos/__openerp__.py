d3cdbbc1b4a0d0daffe22963423bb8fc0d8ed081
Jahkahyah <jahkahyah@xdebianx>
2015-09-22 17:48:20 -0500

    prueba

---
+++ connector_pos/__openerp__.py
@@ -0,0 +1,12 @@
+{   
+    "name"        : "connector_pos",
+    "version"     : "8.0.1.0.0",
+    "category"    : "point_of_sale",
+    "author"      : "Jarsa Sistemas, S.A. de C.V.",
+    "website"     : "www.jarsa.com.mx",
+    "depends"     : [],
+    "summary"     : "Info displayed in module kanban}",
+    "data" : [],
+    "application": True,
+    "installable": True,
+}
