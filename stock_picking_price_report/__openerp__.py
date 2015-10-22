098c14ff3bea2016da30b9a9775d5a28b140b518
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2015-10-22 14:15:54 -0500

    [ADD]: Stock Picking Price Report module

---
+++ stock_picking_price_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Stock Picking Price Report",
+    "version": "8.0.1.0.0",
+    "category": "stock",
+    "author": "Jarsa Sistemas, S.A. de C.V.",
+    "website": "www.jarsa.com.mx",
+    "license": "AGPL-3",
+    "depends": [
+        'stock',
+    ],
+    "summary": "",
+    "description": "test",
+    "data": [
+        'reports/stock_picking_price_report.xml'
+    ],
+    "demo": [
+    ],
+    "application": True,
+    "installable": True,
+}
