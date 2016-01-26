294d2ffaddb1a8a7bf8d64e8d1d02388d2d0e1ea
Luis Triana <luis.triana@jarsa.com.mx>
2016-01-26 13:58:32 -0600

    [FIX]- change in the api

--- website_sale_stock_available/controllers/main.py
+++ website_sale_stock_available/controllers/main.py
@@ -9,16 +9,16 @@ class WebsiteSale(website_sale):
     @http.route(['/shop/product/<model("product.template"):product>'],
                 type='http', auth="public", website=True)
     def product(self, product, category='', search='', **kwargs):
-        cr, uid, context, pool = (request.cr, request.uid,
-                                  request.context, request.registry)
+        cr, uid = (request.cr, request.uid,
+                   request.registry)
         resp = super(WebsiteSale, self).product(product, category,
                                                 search, **kwargs)
 
-        settings_obj = pool.get('stock.config.settings')
+        settings_obj = self.env['stock.config.settings']
         config_ids = settings_obj.search(cr, uid, [], limit=1,
-                                         order='id DESC', context=context)
+                                         order='id DESC')
         if config_ids:
             stock_set = settings_obj.browse(cr, uid,
-                                            config_ids[0], context=context)
+                                            config_ids[0])
             resp.qcontext['min_stock'] = stock_set.group_website_minimum_stock
         return resp
