7db0e6ff9e708be2750ede6475aef27c16096a7e
Jesus Alan Ramos RodrÃiguez <alan.ramos@jarsa.com.mx>
2016-01-29 23:11:28 -0600

    [FIX]: Set unactive Not defined brand and model

---
+++ auth_signup_motomanic/models/fleet_vehicle_model_brand.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © <2016> <Jarsa Sistemas, S.A. de C.V.>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class FleetVehicleModelBrand(models.Model):
+    _name = 'fleet.vehicle.model.brand'
+
+    active = fields.Boolean()
