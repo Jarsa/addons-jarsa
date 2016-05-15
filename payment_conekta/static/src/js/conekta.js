8a5604973aa8243925981a046d0372741b9c4778
Jesus Alan Ramos Rodriguez <alan.ramos@jarsa.com.mx>
2016-05-15 12:43:45 -0500

    [ADD][payment_conekta] algorithm to identify card brand and add images to view

--- payment_conekta/static/src/js/conekta.js
+++ payment_conekta/static/src/js/conekta.js
@@ -1,30 +1,47 @@
 $(function () {
   $("#card-form").submit(function(event) {
     var $form = $(this);
-
-    // Previene hacer submit más de una vez
     $form.find("button").prop("disabled", true);
     Conekta.token.create($form, conektaSuccessResponseHandler, conektaErrorResponseHandler);
-   
-    // Previene que la información de la forma sea enviada al servidor
     return false;
   });
 });
 
 var conektaSuccessResponseHandler = function(token) {
     var $form = $("#card-form");
-
-    /* Inserta el token_id en la forma para que se envíe al servidor */
     $form.append($("<input name='token_id'>").val(token.id));
- 
-    /* and submit */
     $form.get(0).submit();
 };
 
 var conektaErrorResponseHandler = function(response) {
   var $form = $("#card-form");
-  
-  /* Muestra los errores en la forma */
   $form.find(".card-errors").text(response.message);
   $form.find("button").prop("disabled", false);
 };
+
+function getCardBrand() {
+    var card_number = document.getElementById("conekta-card-number");
+    var brand = Conekta.card.getBrand(card_number.value);
+    console.log(brand)
+    if (brand == "visa") {
+        $("#visa").show();
+        $("#mastercard").hide();
+        $("#amex").hide();
+        $("#all-cards").hide();
+    } else if (brand == "mastercard") {
+        $("#visa").hide();
+        $("#mastercard").show();
+        $("#amex").hide();
+        $("#all-cards").hide();
+    } else if (brand == "amex") {
+        $("#visa").hide();
+        $("#mastercard").hide();
+        $("#amex").show();
+        $("#all-cards").hide();
+    } else {
+        $("#visa").hide();
+        $("#mastercard").hide();
+        $("#amex").hide();
+        $("#all-cards").show();
+    }
+}
