051d48cd1c1bc8e427f3d664352dd315b7ac315e
Sarai Osorio <sarai.osorio@jarsa.com.mx>
2016-05-20 18:01:28 +0000

    [ADD][cva] add test of method get_product

--- connector_cva/tests/test_cva_config_settings.py
+++ connector_cva/tests/test_cva_config_settings.py
@@ -7,14 +7,19 @@ from openerp.tests.common import TransactionCase
 
 class TestCvaConfigSettings(TransactionCase):
     """
-    This will test model product product
+    This will test model cva config settings
     """
     def setUp(self):
         """
         Define global variables
         """
         super(TestCvaConfigSettings, self).setUp()
-        self.group = self.env['cva.group']
-        self.cva = self.env['cva.config.settings']
 
-    def test_10_cva_config_settings_set_allowed_groups(self):
+    def test_10_cva_config_settings_get_products(self):
+        cva_obj = self.env['cva.config.settings']
+        cva = cva_obj.create({
+            'name': '40762',
+            'allowed_groups': [(0, 0, {'name': 'AIRE ACONDICIONADO'})],
+        })
+        cva.execute()
+        cva.get_products()
