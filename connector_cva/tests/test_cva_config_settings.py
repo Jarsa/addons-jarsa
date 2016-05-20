2e69a9d7512faed13d876d4f58375fbb454e7fcb
Jesus Alan Ramos Rodriguez <alan.ramos@jarsa.com.mx>
2016-05-20 14:02:06 -0500

    [ADD][connector_cva] test to get_group method use of mock

--- connector_cva/tests/test_cva_config_settings.py
+++ connector_cva/tests/test_cva_config_settings.py
@@ -3,6 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.tests.common import TransactionCase
+from mock import MagicMock
+from lxml import etree
 
 
 class TestCvaConfigSettings(TransactionCase):
@@ -14,12 +16,23 @@ class TestCvaConfigSettings(TransactionCase):
         Define global variables
         """
         super(TestCvaConfigSettings, self).setUp()
+        self.cva = self.env['cva.config.settings']
 
     def test_10_cva_config_settings_get_products(self):
-        cva_obj = self.env['cva.config.settings']
-        cva = cva_obj.create({
+        cva = self.cva.create({
             'name': '40762',
             'allowed_groups': [(0, 0, {'name': 'AIRE ACONDICIONADO'})],
         })
         cva.execute()
         cva.get_products()
+
+    def test_20_cva_config_settings_get_groups(self):
+        xml = ('<articulos><item><grupo>BACK PACK (MOCHILA)</grupo></item>'
+               '</articulos>')
+        cva = self.cva.create({
+            'name': '40762',
+            'main_location': self.env.ref('connector_cva.loc_torreon').id})
+        cva.execute()
+        cva.connect_cva = MagicMock()
+        cva.connect_cva.return_value = etree.XML(xml)
+        cva.get_groups()
