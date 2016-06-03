e890cff46efc237f6d914c3a62c300e97b92e34c
Jesus Alan Ramos Rodriguez <alan.ramos@jarsa.com.mx>
2016-06-02 19:03:31 -0500

    [FIX][payment_conekta] Fix tests

--- payment_conekta/static/src/js/payment_conekta_tour_buy.js
+++ payment_conekta/static/src/js/payment_conekta_tour_buy.js
@@ -1,4 +1,4 @@
-odoo.define('website_sale.tour', function (require) {
+odoo.define('payment_conekta.tour', function (require) {
 'use strict';
 
 var Tour = require('web.Tour');
@@ -10,59 +10,26 @@ Tour.register({
     mode: 'test',
     steps: [
         {
-            title:  "search ipod",
-            element: 'form:has(input[name="search"]) a.a-submit',
-            onload: function() {
-                $('input[name="search"]').val("ipod");
-            }
-        },
-        {
             title:     "select ipod",
             element:   '.oe_product_cart a:contains("iPod")',
         },
         {
-            title:     "select ipod 32GB",
-            waitFor:   '#product_detail',
-            element:   'label:contains(32 GB) input',
-        },
-        {
             title:     "click on add to cart",
-            waitFor:   'label:contains(32 GB) input:propChecked',
             element:   '#product_detail form[action^="/shop/cart/update"] .btn',
         },
         {
-            title:     "add one more iPod",
-            waitFor:   '.my_cart_quantity:contains(2)',
-            element:   '#cart_products tr:contains("32 GB") a.js_add_cart_json:eq(1)',
-        },
-        {
-            title:     "set one iPod",
-            waitNot:   '#cart_products tr:contains("Apple In-Ear Headphones")',
-            element:   '#cart_products input.js_quantity',
-            sampleText: '1',
-        },
-        {
             title:     "go to checkout",
-            waitFor:   '#cart_products input.js_quantity:propValue(1)',
             element:   'a[href="/shop/checkout"]',
         },
         {
-            title:     "test with input error",
-            element:   'form[action="/shop/confirm_order"] .btn:contains("Confirm")',
-            onload: function (tour) {
-                $("input[name='phone']").val("");
-            },
-        },
-        {
             title:     "test without input error",
-            waitFor:   'form[action="/shop/confirm_order"] .has-error',
             element:   'form[action="/shop/confirm_order"] .btn:contains("Confirm")',
             onload: function (tour) {
                 if ($("input[name='name']").val() === "")
                     $("input[name='name']").val("website_sale-test-shoptest");
                 if ($("input[name='email']").val() === "")
                     $("input[name='email']").val("website_sale_test_shoptest@websitesaletest.odoo.com");
-                $("input[name='phone']").val("123");
+                $("input[name='phone']").val("123123123");
                 $("input[name='street2']").val("123");
                 $("input[name='city']").val("123");
                 $("input[name='zip']").val("123");
@@ -76,7 +43,7 @@ Tour.register({
         {
             title:     "Pay Now",
             waitFor:   '#payment_method label:has(input:checked):has(img[title="Conekta"])',
-            element:   '.oe_sale_acquirer_button .btn[type="submit"]:visible',
+            element:   '.oe_sale_acquirer_button .btn[name="conekta"]:visible',
             onload: function(tour){
                 $('input[data-conekta="card[name]"]').val('payment_conekta-test');
                 $('input[data-conekta="card[number]"]').val('4242424242424242');
