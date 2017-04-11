d013af3e33f81aafe46c4c8c6d7713cd019f74ce
Luis Triana <luis.triana@jarsa.com.mx>
2017-04-11 12:18:13 -0500

    [ADD][account_tax_cash_basis_reference]-Module for Reclasificacion of IVA

---
+++ account_tax_cash_basis_reference/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Accounting - Tax Cash Basis Move Assignation",
+    "version": "10.0.0.1.0",
+    "category": "Accounting",
+    "author": "Jarsa Sistemas",
+    "website": "https://www.jarsa.com.mx",
+    "depends": ["account_tax_cash_basis"],
+    "summary": "This module assign the tax moves to the invoice moves",
+    "license": "AGPL-3",
+    "data": [
+        'views/account_move_view.xml',
+    ],
+    "application": True,
+    "installable": True,
+}
