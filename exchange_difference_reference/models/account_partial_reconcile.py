5e8c36aeb05496ed6c0d9c555ecc65a5018fd356
oscar <oscar.garza@jarsa.com.mx>
2017-05-04 11:54:59 -0500

    [FIX][exchange_difference_reference] - Compute the tax amounts with the correct currency rate.

--- exchange_difference_reference/models/account_partial_reconcile.py
+++ exchange_difference_reference/models/account_partial_reconcile.py
@@ -2,31 +2,81 @@
 # Copyright 2017, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models
+from openerp import _, api, models
 
 
 class AccountPartialReconcileCashBasis(models.Model):
     _inherit = 'account.partial.reconcile'
 
-    def create_exchange_rate_entry(self, aml_to_fix, amount_diff,
-                                   diff_in_currency, currency, move_date):
-        line_to_reconcile, partial_rec = super(
-            AccountPartialReconcileCashBasis, self).create_exchange_rate_entry(
-            aml_to_fix, amount_diff, diff_in_currency, currency, move_date)
-        reconcile_move_line = self.env['account.move.line'].browse(
-            line_to_reconcile)
-        partial_reconcile = self.browse(partial_rec)
-        # We need get the customer / supplier invoice of the main partial
-        # reconcile, so, we check the type of the journal to know the
-        # invoice type
-        reference = (
-            partial_reconcile.debit_move_id.move_id.rate_diff_partial_rec_id.
-            debit_move_id.move_id.name if
-            partial_reconcile.debit_move_id.move_id.rate_diff_partial_rec_id.
-            debit_move_id.move_id.journal_id.type == 'sale' else
-            partial_reconcile.credit_move_id.move_id.rate_diff_partial_rec_id.
-            debit_move_id.move_id.name)
-        reconcile_move_line.move_id.write({
-            'ref': reference
-            })
-        return line_to_reconcile, partial_rec
+    @api.model
+    def create(self, vals):
+        res = super(AccountPartialReconcileCashBasis, self).create(vals)
+        move_tax = self.env['account.move'].search(
+            [('tax_cash_basis_rec_id', '=', res.id)])
+        if move_tax:
+            # Get the tax payment lines
+            tax_payment_lines = move_tax.line_ids.filtered(
+                lambda x: x.tax_line_id)
+            move_lines = []
+            # Get the exchange difference move
+            exchange_move_id = self.env['account.move'].search(
+                [('rate_diff_partial_rec_id', '=', res.id)])
+            if exchange_move_id:
+                # Get the axchange difference and the exchange difference
+                # account
+                exchange_difference = exchange_move_id.amount
+                exchange_account_id = exchange_move_id.dummy_account_id
+
+                for tax_line in tax_payment_lines:
+                    # Get the tax rate and the base rate for the compute
+                    tax_rate = tax_line.tax_line_id.amount / 100
+                    if tax_rate < 0.0:
+                        base = tax_line.tax_line_id.amount / 100 - 1.0
+                    else:
+                        base = tax_line.tax_line_id.amount / 100 + 1.0
+                    # Compute the tax difference
+                    tax_difference = (exchange_difference / base) * tax_rate
+                    # Edit the original tax payment line with the real amount
+                    move_lines.append(
+                        (1, tax_line.id, {
+                            'debit': (
+                                tax_line.debit - tax_difference if
+                                tax_line.debit > 0.0 else 0.0),
+                            'credit': (
+                                tax_line.credit - tax_difference if
+                                tax_line.credit > 0.0 else 0.0),
+                        }))
+                    # Create a counterpart with the corresponding currency
+                    # exchange ccount
+                    move_lines.append(
+                        (0, 0, {
+                            'account_id': exchange_account_id.id,
+                            'debit': (
+                                tax_difference if
+                                tax_line.debit > 0.0 else 0.0),
+                            'credit': (
+                                tax_difference if
+                                tax_line.credit > 0.0 else 0.0),
+                            'journal_id': tax_payment_lines[0].journal_id.id,
+                            'partner_id': tax_payment_lines[0].partner_id.id,
+                            'name': _(
+                                ('Exchange Currency %s') % (
+                                    tax_line.tax_line_id.name)),
+                        }))
+                move_tax.button_cancel()
+                move_tax.write({
+                    'line_ids': [x for x in move_lines],
+                })
+                move_tax.post()
+
+                # We get the invoice number to put it in the exchange
+                # difference journal entry
+                reference = (
+                    res.debit_move_id.move_id.name if
+                    res.debit_move_id.move_id.journal_id.type == 'sale' else
+                    res.debit_move_id.move_id.name)
+                exchange_move_id.button_cancel()
+                exchange_move_id.write({
+                    'ref': reference,
+                    })
+        return res
