9475cf6f1029ab7b40810cfb8026580900194ed7
Oscar Garza <oscar.garza@jarsa.com.mx>
2017-06-20 11:23:52 -0500

    [ADD][helpdesk_open_ticket] - Module to re open closed / cancelled tickets.

---
+++ helpdesk_open_ticket/models/mail_message.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class MailMessage(models.Model):
+    _inherit = 'mail.message'
+
+    @api.model
+    def create(self, vals):
+        active_model = vals.get('model', False)
+        if active_model == 'helpdesk.ticket':
+            ticket = self.env[active_model].browse(vals['res_id'])
+            if (ticket.stage_id.is_close and
+                    vals['message_type'] != 'notification'):
+                stage = self.env['helpdesk.stage'].search(
+                    [('team_ids', 'in', ticket.team_id.id)],
+                    order='sequence', limit=1)
+                ticket.stage_id = stage.id or False
+                ticket.user_id = False
+        return super(MailMessage, self).create(vals)
