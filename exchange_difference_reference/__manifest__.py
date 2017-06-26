51e1212f7e7daed6781e20e25b63819250a5fab4
Oscar Garza <oscar.garza@jarsa.com.mx>
2017-06-26 18:45:33 -0500

    [FIX][exchange_difference_reference] - Exchange currency rate difference with tax cash basis.

--- exchange_difference_reference/__manifest__.py
+++ exchange_difference_reference/__manifest__.py
@@ -3,15 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Accounting - Exchange Difference Reference Assignation",
+    "name": (
+        "Accounting - Exchange Difference Reference Assignation"
+        " / Exchange Currency TAX amount FIX"),
     "version": "10.0.0.1.0",
     "category": "Accounting",
     "author": "Jarsa Sistemas",
     "website": "https://www.jarsa.com.mx",
-    "depends": ["account_tax_cash_basis"],
+    "depends": ["account", "account_tax_cash_basis"],
     "summary": (
         "This module add the name of the invoice name"
-        " to the exchange reference journal entry"),
+        " to the exchange reference journal entry and fix the tax amount"
+        " corresponding to the exchange currency difference."),
     "license": "AGPL-3",
     "data": [],
     "application": False,
--- exchange_difference_reference/models/account_partial_reconcile.py
+++ exchange_difference_reference/models/account_partial_reconcile.py
@@ -8,6 +8,81 @@ from odoo import _, api, models
 class AccountPartialReconcileCashBasis(models.Model):
     _inherit = 'account.partial.reconcile'
 
+    def create_exchange_rate_entry(self, aml_to_fix, amount_diff,
+                                   diff_in_currency, currency, move_date):
+        for rec in self:
+            line_to_reconcile, partial_rec = (
+                super(AccountPartialReconcileCashBasis,
+                      self).create_exchange_rate_entry(
+                    aml_to_fix, amount_diff, diff_in_currency,
+                    currency, move_date))
+            move = line_to_reconcile.move_id
+            # We get the invoice and the payment move
+            invoice_move = (
+                    rec.credit_move_id if
+                    rec.credit_move_id.journal_id.type == 'purchase' else
+                    rec.debit_move_id)
+            bank_move = (
+                rec.debit_move_id.move_id if
+                rec.credit_move_id.journal_id.type == 'purchase' else
+                rec.credit_move_id)
+            # We loop the tax lines of the invoice move to get the tax rate
+            for tax_line in invoice_move.move_id.line_ids.filtered(
+                    lambda r: r.tax_ids):
+                lines = []
+                for tax in tax_line.tax_ids.filtered(
+                        lambda x: x.use_cash_basis):
+                    # We check if the move will be a amount_currency fix
+                    # if this is True we compute the currency amount
+                    # to the correspinding currency.
+                    if diff_in_currency != 0:
+                        amount_diff = (
+                            currency.with_context(date=bank_move.date).compute(
+                                diff_in_currency, rec.company_currency_id))
+                    # We get the tax difference based in the base amount
+                    tax_amount_diff = (
+                        (amount_diff /
+                         (tax.amount * .01 + 1) * (tax.amount * .01)))
+                    # We create the tax counterpart
+                    lines.append((0, 0, {
+                        'name': (_(
+                            'Currency exchange rate difference for: ' +
+                            tax.name)),
+                        'debit': (
+                            tax_amount_diff < 0 and -tax_amount_diff or 0.0),
+                        'credit': (
+                            tax_amount_diff > 0 and tax_amount_diff or 0.0),
+                        'account_id': tax.cash_basis_account.id,
+                        'move_id': move.id,
+                        'currency_id': currency.id,
+                        'partner_id': rec.debit_move_id.partner_id.id,
+                    }))
+                    # We create the gain / loss counterpart
+                    lines.append((0, 0, {
+                        'name': (_(
+                            'Currency exchange rate difference for: ' +
+                            tax.name)),
+                        'debit': (
+                            tax_amount_diff > 0 and tax_amount_diff or 0.0),
+                        'credit': (
+                            tax_amount_diff < 0 and -tax_amount_diff or 0.0),
+                        'account_id': (
+                            tax_amount_diff > 0 and
+                            rec.company_id.currency_exchange_journal_id.
+                            default_debit_account_id.id or
+                            rec.company_id.currency_exchange_journal_id.
+                            default_credit_account_id.id),
+                        'move_id': move.id,
+                        'currency_id': currency.id,
+                        'partner_id': rec.debit_move_id.partner_id.id,
+                    }))
+                move.button_cancel()
+                move.write({
+                    'line_ids': [x for x in lines],
+                })
+                move.post()
+        return line_to_reconcile, partial_rec
+
     @api.model
     def create(self, vals):
         res = super(AccountPartialReconcileCashBasis, self).create(vals)
