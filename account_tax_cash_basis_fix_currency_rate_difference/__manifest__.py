bb3b7c3faf0381f94075447c7d82cd86313dae5d
Oscar Garza <oscar.garza@jarsa.com.mx>
2017-06-27 09:24:52 -0500

    [REF][exchange_difference_reference] - reference of the currency exchange journal entry

--- account_tax_cash_basis_fix_currency_rate_difference/__manifest__.py
+++ account_tax_cash_basis_fix_currency_rate_difference/__manifest__.py
@@ -3,18 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": (
-        "Accounting - Exchange Difference Reference Assignation"
-        " / Exchange Currency TAX amount FIX"),
+    "name": "Account Tax Cash Basis - Fix Currency Rate Difference",
     "version": "10.0.0.1.0",
     "category": "Accounting",
     "author": "Jarsa Sistemas",
     "website": "https://www.jarsa.com.mx",
-    "depends": ["account", "account_tax_cash_basis"],
+    "depends": ["account_tax_cash_basis"],
     "summary": (
         "This module add the name of the invoice name"
-        " to the exchange reference journal entry and fix the tax amount"
-        " corresponding to the exchange currency difference."),
+        " to the exchange reference journal entry and fix "
+        "the currency rate difference with tax cash basis behavior."),
     "license": "AGPL-3",
     "data": [],
     "application": False,
