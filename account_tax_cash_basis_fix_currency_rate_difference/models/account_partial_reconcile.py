92a0369321688422ba523e437f05a49a9b44f2c5
Oscar Garza <oscar.garza@jarsa.com.mx>
2017-06-27 12:29:02 -0500

    [REF][exchange_difference] - logic

--- account_tax_cash_basis_fix_currency_rate_difference/models/account_partial_reconcile.py
+++ account_tax_cash_basis_fix_currency_rate_difference/models/account_partial_reconcile.py
@@ -29,7 +29,8 @@ class AccountPartialReconcileCashBasis(models.Model):
                 rec.credit_move_id)
             # We loop the tax lines of the invoice move to get the tax rate
             for tax in invoice_move.move_id.line_ids.filtered(
-                    lambda r: r.tax_line_id.use_cash_basis):
+                    lambda r: r.tax_line_id.use_cash_basis).mapped(
+                    "tax_line_id"):
                 lines = []
                 # We check if the move will be a amount_currency fix
                 # if this is True we compute the currency amount
